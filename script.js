a12c2dd5b9429d6db1ae9ec7a9cac6b7d70ef993
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-11-10 09:40:31 +0100

    terminated

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+// Chiedi all’utente il suo nome,
+const nomeUtente = ( prompt ('Iserisci il Nome' ));
+// poi chiedi il suo cognome,
+const cognomeUtente = ( prompt ('Inserisci il Cognome' ));
+// poi chiedi il suo colore preferito
+const coloreUtente = ( prompt ('Inserici il tuo colore preferito' ));
+// Infine scrivi sulla pagina nomecognomecolorepreferito21 (con i dati dell'utente, gentilmente)
+document.getElementById('output').innerHTML = nomeUtente +' '+ cognomeUtente +' '+ coloreUtente +'21';
+
+
+
+
